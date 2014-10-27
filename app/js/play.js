9290fb3bb5a3c940b270d348184b406123a5c5f2
Gizmmo <travisscott301@gmail.com>
2014-10-27 16:48:27 -0500

    Updates the create function logic to use more functions to clean up logic

--- app/js/play.js
+++ app/js/play.js
@@ -9,80 +9,18 @@ var playState = {
 	},
 
 	create: function() {
+		this.createPlayer(); //Creates a player and adds gravity
+		this.createWorld(); //Create the world for the player to move around in
+		this.createEnemies(); //Creates the enemies in the world
+		this.createEmitter(); //Creates the particle emitter for the player death
+		this.createCoin(); //Creates the coin in the game world
+		this.createScore(); //Creates the score label
+		this.createKeyInputs(); //Creates inputs for the player
 
-		//Create local variable
-		this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
-		this.player.anchor.setTo(0.5, 0.5);
-		this.cursor = game.input.keyboard.createCursorKeys();
-		this.createWorld();
-		this.enemies = game.add.group();
-		this.enemies.enableBody = true;
+		this.addPlayerAnimations(); //Adds animations to the player object
 
-		//Create the emitter with 15 particles.  We dont need to set the x and y
-		//since we don't know where to do the explosion yet
-		this.emitter = game.add.emitter(0, 0, 15);
+		this.addSounds(); //Adds sounds to various things
 
-		//Set the pixel image for the particles
-		this.emitter.makeParticles('pixel');
-
-		//Set the y speed of the particles between -150 and 150
-		//The speed will randomly be picked between this for each particle
-		this.emitter.setYSpeed(-150, 150);
-
-		//Do the same for the x speed
-		this.emitter.setXSpeed(-150, 150);
-
-		//Use no gravity for the particles
-		this.emitter.gravity = 0;
-
-		//Create the 'right' animation of the player by looping frames 1 and 2
-		this.player.animations.add('right', [1, 2], 8, true);
-
-		//Create the 'left' animations of the player by looping frames 3 and 4
-		this.player.animations.add('left', [3, 4], 8, true);
-
-		this.jumpSound = game.add.audio('jump');
-		this.coinSound = game.add.audio('coin');
-		this.deadSound = game.add.audio('dead');
-
-		//Create 10 enemies with the enemy image in the group
-		//The enemies are dead by default, so they are not visible in game
-		this.enemies.createMultiple(10, 'enemy');
-
-		///Containes the time of the next enemy creation
-		this.nextEnemy = 0;
-
-		//Display the coin
-		this.coin = game.add.sprite(60, 140, 'coin');
-
-		//Add Arcade Physics to coin
-		game.physics.arcade.enable(this.coin);
-
-		//Set the anchor point of the coin to the center
-		this.coin.anchor.setTo(0.5, 0.5);
-
-		//Tell Phaser that the player will use the Arcade physics engine
-		game.physics.arcade.enable(this.player);
-
-		//Add verticle gravity to the player
-		this.player.body.gravity.y = 500;
-
-		//Display the score
-		this.scoreLabel = game.add.text(30, 30, 'score: 0', {
-			font: '18px Arial',
-			fill: '#fffff'
-		});
-
-		game.global.score = 0;
-
-		//Captures key buttons so that the browser does not
-		game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT])
-
-		this.wasd = {
-			up: game.input.keyboard.addKey(Phaser.Keyboard.W),
-			left: game.input.keyboard.addKey(Phaser.Keyboard.A),
-			right: game.input.keyboard.addKey(Phaser.Keyboard.D)
-		}
 
 	},
 
@@ -103,15 +41,17 @@ var playState = {
 		this.movePlayer();
 
 		//If the nextEnemy time has passed
-		if(this.nextEnemy < game.time.now) {
+		if (this.nextEnemy < game.time.now) {
 
 			//Define some variables were going to use
-			var start = 4000, end = 1000, score = 100;
+			var start = 4000,
+				end = 1000,
+				score = 100;
 
 			//Formula to decrease the delay between enemies over time
 			//at first its 4000ms then slowly reaches 1000 ms
-			
-			var delay = Math.max(start - (start - end) * game.global.score/score, end);
+
+			var delay = Math.max(start - (start - end) * game.global.score / score, end);
 			//We Add an enemy
 			this.addEnemy();
 
@@ -141,6 +81,96 @@ var playState = {
 		enemy.body.bounce.x = 1;
 		enemy.checkWorldBounds = true;
 		enemy.outOfBoundsKill = true;
+	},
+
+	addPlayerAnimations: function() {
+		//Create the 'right' animation of the player by looping frames 1 and 2
+		this.player.animations.add('right', [1, 2], 8, true);
+
+		//Create the 'left' animations of the player by looping frames 3 and 4
+		this.player.animations.add('left', [3, 4], 8, true);
+	},
+
+	addSounds: function() {
+		this.jumpSound = game.add.audio('jump');
+		this.coinSound = game.add.audio('coin');
+		this.deadSound = game.add.audio('dead');
+	},
+
+	createCoin: function() {
+		//Display the coin
+		this.coin = game.add.sprite(60, 140, 'coin');
+
+		//Add Arcade Physics to coin
+		game.physics.arcade.enable(this.coin);
+
+		//Set the anchor point of the coin to the center
+		this.coin.anchor.setTo(0.5, 0.5);
+	},
+
+	createEmitter: function() {
+		//Create the emitter with 15 particles.  We dont need to set the x and y
+		//since we don't know where to do the explosion yet
+		this.emitter = game.add.emitter(0, 0, 15);
+
+		//Set the pixel image for the particles
+		this.emitter.makeParticles('pixel');
+
+		//Set the y speed of the particles between -150 and 150
+		//The speed will randomly be picked between this for each particle
+		this.emitter.setYSpeed(-150, 150);
+
+		//Do the same for the x speed
+		this.emitter.setXSpeed(-150, 150);
+
+		//Use no gravity for the particles
+		this.emitter.gravity = 0;
+	},
+
+	createEnemies: function() {
+		this.enemies = game.add.group();
+		this.enemies.enableBody = true;
+
+		//Create 10 enemies with the enemy image in the group
+		//The enemies are dead by default, so they are not visible in game
+		this.enemies.createMultiple(10, 'enemy');
+
+		///Containes the time of the next enemy creation
+		this.nextEnemy = 0;
+	},
+
+	createKeyInputs: function() {
+		this.cursor = game.input.keyboard.createCursorKeys();
+
+		//Captures key buttons so that the browser does not
+		game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT])
+
+		this.wasd = {
+			up: game.input.keyboard.addKey(Phaser.Keyboard.W),
+			left: game.input.keyboard.addKey(Phaser.Keyboard.A),
+			right: game.input.keyboard.addKey(Phaser.Keyboard.D)
+		}
+	},
+
+	createPlayer: function() {
+		this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+		this.player.anchor.setTo(0.5, 0.5);
+
+		//Tell Phaser that the player will use the Arcade physics engine
+		game.physics.arcade.enable(this.player);
+
+		//Add verticle gravity to the player
+		this.player.body.gravity.y = 500;
+	},
+
+	createScore: function() {
+		//Display the score
+		this.scoreLabel = game.add.text(30, 30, 'score: 0', {
+			font: '18px Arial',
+			fill: '#fffff'
+		});
+
+		game.global.score = 0;
 	},
 
 	createWorld: function() {
@@ -193,7 +223,7 @@ var playState = {
 
 	playerDie: function() {
 
-		if(!this.player.alive) {
+		if (!this.player.alive) {
 			return;
 		}
 		//Kill the player to make it dissappear from the screen
@@ -206,7 +236,7 @@ var playState = {
 
 		//Start the emitter, by exploding 15 particles that will live for 600ms
 		this.emitter.start(true, 600, null, 15);
-		
+
 		//Cal the startMenu function after 1000ms
 		game.time.events.add(1000, this.startMenu, this);
 	},
@@ -217,7 +247,13 @@ var playState = {
 
 	takeCoin: function(player, coin) {
 		//Player grows when grabbing a coin
-		game.add.tween(this.player.scale).to({x: 1.3, y: 1.3}, 50).to({x: 1, y: 1}, 150).start();
+		game.add.tween(this.player.scale).to({
+			x: 1.3,
+			y: 1.3
+		}, 50).to({
+			x: 1,
+			y: 1
+		}, 150).start();
 
 		//Increase score by 5
 		game.global.score += 5;
@@ -234,7 +270,10 @@ var playState = {
 	updateCoinPosition: function() {
 		//The coin grows into position
 		this.coin.scale.setTo(0, 0);
-		game.add.tween(this.coin.scale).to({x: 1, y: 1}, 300).start();
+		game.add.tween(this.coin.scale).to({
+			x: 1,
+			y: 1
+		}, 300).start();
 
 
 		var coinPosition = [{
