33d323c39a38a12ba388325a218a0074717ffe46
Gizmmo <travisscott301@gmail.com>
2014-10-26 19:54:55 -0500

    Created world, added physics to all objects, and had the game start over on player leaving the screen

--- app/main.js
+++ app/main.js
@@ -9,6 +9,8 @@ var mainState = {
 		// That's where we load the game's assets
 		
 		game.load.image('player', 'assets/player.png');
+		game.load.image('wallV', 'assets/wallVertical.png');
+		game.load.image('wallH', 'assets/wallHorizontal.png');
 
 	},
 	create: function() {
@@ -23,6 +25,7 @@ var mainState = {
 		 this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
 		 this.player.anchor.setTo(0.5, 0.5);
 		 this.cursor = game.input.keyboard.createCursorKeys();
+		 this.createWorld();
 
 		 //Tell Phaser that the player will use the Arcade physics engine
 		 game.physics.arcade.enable(this.player);
@@ -35,7 +38,40 @@ var mainState = {
 		// This function is called 60 times per second 
 		// It contains the game's logic
 		
+		//Tell Phaser that the player and walls should collide
+		game.physics.arcade.collide(this.player, this.walls);
+
 		this.movePlayer();
+
+		if(!this.player.inWorld) {
+			this.playerDie();
+		}
+	},
+
+	createWorld: function () {
+		//Create our wall group with Arcade physics
+		this.walls = game.add.group();
+		this.walls.enableBody = true;
+
+		//Create the 10 walls
+		game.add.sprite(0, 0, 'wallV', 0, this.walls); //Left
+		game.add.sprite(480, 0, 'wallV', 0, this.walls); //Left
+
+		game.add.sprite(0, 0, 'wallH', 0, this.walls); //Top left
+		game.add.sprite(300, 0, 'wallH', 0, this.walls); //Top right
+		game.add.sprite(0, 320, 'wallH', 0, this.walls); //Bottom left
+		game.add.sprite(300, 320, 'wallH', 0, this.walls); //Bottom Right
+
+		game.add.sprite(-100, 160, 'wallH', 0, this.walls); //Middle left
+		game.add.sprite(400, 160, 'wallH', 0, this.walls); //Middle Right
+
+		var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls);
+		middleTop.scale.setTo(1.5, 1);
+		var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls);
+		middleBottom.scale.setTo(1.5, 1);
+
+		//Set all the walls to be immovable
+		this.walls.setAll('body.immovable', true);
 	},
 
 	movePlayer: function () {
@@ -62,6 +98,10 @@ var mainState = {
 			//Move the player upward (jump)
 			this.player.body.velocity.y = -320;
 		}
+	},
+
+	playerDie: function() {
+		game.state.start('main');
 	}
 
 };
