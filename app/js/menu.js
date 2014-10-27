e48c43a9a493ce7c4d1e0b4ad6caa1fcc5007800
Gizmmo <travisscott301@gmail.com>
2014-10-26 21:36:33 -0500

    Filled in the menu state and the loading to the play state

---
+++ app/js/menu.js
@@ -0,0 +1,28 @@
+var menuState = {
+	create: function () {
+		//Add a background image
+		game.add.image(0, 0, 'background');
+
+		//Display the name of the game
+		var nameLabel = game.add.text(game.world.centerX, 80, 'Super Coin Box', {font: '60px Arial', fill: '#ffffff'});
+		nameLabel.anchor.setTo(0.5, 0.5);
+
+		//Show the score at the center of the screen
+		var scoreLabel = game.add.text(game.world.centerX, game.world.centerY, 'score: ' + game.global.score, {font: '25px Arial', fill: '#fffff'})
+		scoreLabel.anchor.setTo(0.5, 0.5);
+
+		//Explain how to start the game
+		var startLabel = game.add.text(game.world.centerX, game.world.height-80, 'press the up arrow key to start', {font: '25px Arial', fill: '#ffffff'});
+		startLabel.anchor.setTo(0.5, 0.5);
+
+		var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+
+		//Create a new Phaser keyboard variable: the up arrow key
+		upKey.onDown.addOnce(this.start, this);
+	},
+
+	start: function () {
+		//Start the actual game
+		game.state.start('play');
+	}
+}
