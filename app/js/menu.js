0d83b5e06f83cc9f93f6266d435d2d5881999ffd
Gizmmo <travisscott301@gmail.com>
2014-10-27 12:57:32 -0500

    Menu extended to use more functions for easier reading and editing

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,22 +1,63 @@
 var menuState = {
 	create: function() {
 
+		this.loadLocalStorage();
+		this.setHighScore();
+		this.createBackgroundImage();
+		this.setUpMenuLabels();
+		this.setUpInput();
+
+		//Add the mute button that calls the toggleSound function when pressed
+		this.muteButton = game.add.button(20, 20, 'mute', this.toggleSound, this);
+
+		//If the mouse is over the button it becomes a hand cursor
+		this.muteButton.input.useHandCursor = true;
+
+		//If the game is already muted
+		if (game.sound.mute) {
+			//Change the frame to already display the mute symbol
+			this.muteButton.frame = 1;
+		}
+	},
+
+	createBackgroundImage: function() {
+		//Add a background image
+		game.add.image(0, 0, 'background');
+	},
+
+	start: function() {
+		//Start the actual game
+		game.state.start('play');
+	},
+
+	toggleSound: function() {
+		//Switch the Pahser sound variable from true to flase, or false
+		//to true.
+		game.sound.mute = !game.sound.mute;
+
+		//Change the frame of the button
+		this.muteButton.frame = game.sound.mute ? 1 : 0;
+	},
+
+	loadLocalStorage: function() {
+
 		//If bestScore is not defined it
 		//means that this is the first time the game is being played
 		if (!localStorage.getItem('bestScore')) {
 			//Then set the best score to 0
 			localStorage.setItem('bestScore', 0);
 		}
+	},
 
+	setHighScore: function() {
 		//If the score is higher then the best score
 		if (game.global.score > localStorage.getItem('bestScore')) {
 			//then update the best score
 			localStorage.setItem('bestScore', game.global.score);
 		}
+	},
 
-		//Add a background image
-		game.add.image(0, 0, 'background');
-
+	setUpMenuLabels: function() {
 		//Display the name of the game
 		var nameLabel = game.add.text(game.world.centerX, -50, 'Super Coin Box', {
 			font: '50px Geo',
@@ -37,11 +78,13 @@ var menuState = {
 		})
 		scoreLabel.anchor.setTo(0.5, 0.5);
 
+
 		if (game.device.desktop) {
 			text = 'press the up arrow key to start';
 		} else {
 			var text = 'touch the screen to start';
 		}
+
 		//Explain how to start the game
 		var startLabel = game.add.text(game.world.centerX, game.world.height - 80, text, {
 			font: '25px Geo',
@@ -54,38 +97,15 @@ var menuState = {
 		}, 500).to({
 			angle: 2
 		}, 500).loop().start();
+	},
 
+	setUpInput: function() {
+		//Store the up arrow into a variable
 		var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
 
 		//Create a new Phaser keyboard variable: the up arrow key
 		upKey.onDown.addOnce(this.start, this);
 		//Used for touch events
 		game.input.onDown.addOnce(this.start, this)
-
-		//Add the mute button that calls the toggleSound function when pressed
-		this.muteButton = game.add.button(20, 20, 'mute', this.toggleSound, this);
-
-		//If the mouse is over the button it becomes a hand cursor
-		this.muteButton.input.useHandCursor = true;
-
-		//If the game is already muted
-		if(game.sound.mute) {
-			//Change the frame to already display the mute symbol
-			this.muteButton.frame = 1;
-		}
- 	},
-
-	start: function() {
-		//Start the actual game
-		game.state.start('play');
-	},
-
-	toggleSound: function() {
-		//Switch the Pahser sound variable from true to flase, or false
-		//to true.
-		game.sound.mute = !game.sound.mute;
-
-		//Change the frame of the button
-		this.muteButton.frame = game.sound.mute ? 1 : 0;
 	}
 }
